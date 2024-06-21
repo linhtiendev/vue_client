041c86e585356b3f06acbe4cf3c1186dc724584e
linhtiendev <lelinhtien0707@gmail.com>
2024-06-21 14:07:53 +0700

    feat: setup and design client

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,112 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="dashboard">
+    <AppHeader />
+    <main>
+      <Toolbar />
+      <Team />
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import AppHeader from "@/components/AppHeader.vue";
+import Toolbar from "@/components/Toolbar.vue";
+import Team from "@/components/Team.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader,
+    Toolbar,
+    Team
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  --primary: #6297dd;
+  --primary-alt: #094ca3;
+  --light: #EEEEEE;
+  --light-alt: #F8F8F8;
+  --grey: #888888;
+  --dark: #131A26;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Brush Script MT', cursive;
+
+}
+
+body {
+  background-color: var(--light);
+}
+
+main {
+  width: 100vw;
+  overflow: hidden;
+}
+
+.button {
+  display: inline-block;
+  padding: 8px 16px;
+  background-color: var(--primary);
+  color: #fff;
+  font-size: 20px;
+  font-weight: 700;
+  text-transform: uppercase;
+  cursor: pointer;
+  border-radius: 6px;
+  transition: 0.4s;
+  border: none;
+}
+
+.button:hover {
+  background-color: var(--primary-alt) !important;
+  transform: scale(1.05);
+}
+
+.button.button-outline {
+  background-color: transparent;
+  border: 2px solid var(--primary);
+  padding: 6px 14px;
+  color: var(--primary);
+}
+
+.button.button-outline:hover {
+  background-color: var(--primary-alt) !important;
+  color: #fff;
+  transform: scale(1.05);
+  border: 2px solid var(--light-alt);
+}
+
+.button.button-small {
+  padding: 4px 8px;
+  font-size: 18px;
+  font-weight: 600;
+}
+
+.button.button-alert {
+  background-color: crimson;
+}
+
+.button-group {
+  display: flex;
+  margin: 0px -8px;
+  /* gap: inherit; */
+  gap: 10px;
+}
+
+.button-group.group-end {
+  justify-content: flex-end;
+  gap: 10px;
+}
+
+.button-group.button {
+  margin: 0px 8px;
 }
 </style>
---
+++ src/components/Team.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="team">
+        <div class="team-member table">
+            <div class="table-header table-row">
+                <div class="table-col">Name</div>
+                <div class="table-col">Email</div>
+                <div class="table-col table-actions">Actions</div>
+            </div>
+            <div class="member table-row">
+                <div class="table-col name">Le Linh Tien</div>
+                <div class="table-col email">lelinhtien@gmail.com</div>
+                <div class="table-col actions">
+                    <div class="button-group group-end">
+                        <div class="button button-small button-update">Update</div>
+                        <div class="button button-small button-alert">Delete</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.team {
+    padding: 16px;
+}
+
+.table-row {
+    display: flex;
+    align-items: center;
+}
+
+.table-col {
+    flex: 1 1 33.333%;
+    padding: 16px;
+    color: var(--grey);
+}
+
+.table-row {
+    background-color: #fff;
+}
+
+.table-row:nth-child(2n+1) {
+    background-color: var(--light-alt);
+}
+
+.table .table-header {
+    background-color: var(--dark);
+}
+
+.table .table-header .table-col {
+    color: var(--light);
+}
+
+.table-actions {
+    text-align: right;
+}
+
+.button-update {
+    background-color: green !important;
+}
+</style>
